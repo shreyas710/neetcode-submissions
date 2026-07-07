cb21d41918510ab957d36a930c996889ed78ae2e
Shreyas Kulkarni <shreyasnkk@gmail.com>
2026-07-07 16:13:20 -0700

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n == 0) return 0;
+
+        vector<int> lmax(n), rmax(n);
+
+        lmax[0] = height[0];
+        for (int i = 1; i < n; ++i) {
+            lmax[i] = max(lmax[i - 1], height[i]);
+        }
+
+        rmax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rmax[i] = max(rmax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += min(lmax[i], rmax[i]) - height[i];
+        }
+
+        return ans;
+    }
+};
